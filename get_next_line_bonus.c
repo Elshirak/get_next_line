11b88ec4c12a177cdd0f0ebad5e64cd85bc42e67
Elbakyan Shirak <el.shirak@internet.ru>
2023-05-10 12:45:46 +0400

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static char	*ft_get_slice(int fd, char *few_buffers)
 {
